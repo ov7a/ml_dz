3c4acc71dc571236e810bc43ba4c9f80c1b84fd0
ov7a <ov7a@yandex.ru>
2016-03-03 17:48:40 +0300

    minor: sorting results in find_bests

--- dz2/find_bests.py
+++ dz2/find_bests.py
@@ -51,29 +51,35 @@ classifiers = [
 to_binary = np.vectorize(lambda x: 1.0 if x>=0.5 else 0.0)
 
 ds = pandas.read_csv(sys.argv[1], ';').query("CLASS != 'U'")
-with open(sys.argv[2], "w") as fout:
-	for param1, param2 in itertools.combinations(all_params, 2):
-		X = ds[[param1, param2]] 
-		y = ds['CLASS'].apply(lambda x: 0 if x=='G' else 1)
-		X = StandardScaler().fit_transform(X)
-		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4)
+bests = []
+for param1, param2 in itertools.combinations(all_params, 2):
+	X = ds[[param1, param2]] 
+	y = ds['CLASS'].apply(lambda x: 0 if x=='G' else 1)
+	X = StandardScaler().fit_transform(X)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4)
 
-		best_max_diff = (0, None, None)
-		for name, clf in zip(names, classifiers):
-			clf.fit(X_train, y_train)		
-			y_predicted = clf.predict_proba(X_test)[:,1] 	
-			y_predicted_binary = to_binary(y_predicted)
+	best_max_diff = (0, None, None)
+	for name, clf in zip(names, classifiers):
+		clf.fit(X_train, y_train)		
+		y_predicted = clf.predict_proba(X_test)[:,1] 	
+		y_predicted_binary = to_binary(y_predicted)
 
-			score = accuracy_score(y_predicted_binary, y_test) 
+		score = accuracy_score(y_predicted_binary, y_test) 
+	
+		actual, predictions = y_test.as_matrix(), y_predicted
+		false_positive_rate, true_positive_rate, thresholds = roc_curve(actual, predictions)
+		roc_auc = auc(false_positive_rate, true_positive_rate)
 		
-			actual, predictions = y_test.as_matrix(), y_predicted
-			false_positive_rate, true_positive_rate, thresholds = roc_curve(actual, predictions)
-			roc_auc = auc(false_positive_rate, true_positive_rate)
+		diffs = (true_positive_rate - false_positive_rate)
+	
+		max_diff_index = diffs.argmax() 
+		max_diff_value = diffs[max_diff_index]
+		if (max_diff_value > best_max_diff[0]):
+			best_max_diff = (max_diff_value, thresholds[max_diff_index], name)
 			
-			diffs = (true_positive_rate - false_positive_rate)
-		
-			max_diff_index = diffs.argmax() 
-			max_diff_value = diffs[max_diff_index]
-			if (max_diff_value > best_max_diff[0]):
-				best_max_diff = (max_diff_value, thresholds[max_diff_index], name)
-		fout.write("Best diff for (%s, %s) is %0.2f with threshold %0.2f for %s classifier.\n" % (param1, param2, best_max_diff[0], best_max_diff[1], best_max_diff[2])) 	
+	bests.append((param1, param2, best_max_diff[0], best_max_diff[1], best_max_diff[2]))			
+	
+bests.sort(key = lambda x: x[2])	
+with open(sys.argv[2], "w") as fout:
+	for best in bests:
+		fout.write("Best diff for (%s, %s) is %0.2f with threshold %0.2f for %s classifier.\n" % best) 	
